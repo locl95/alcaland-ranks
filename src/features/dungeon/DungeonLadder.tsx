098e9cfcb8cfad631677e5387540a79837796ada
eric lopez <e.4p.bigdata.tid@telefonica.com>
2023-11-15 19:38:30 +0100

    Implement clickable url

--- src/features/dungeon/DungeonLadder.tsx
+++ src/features/dungeon/DungeonLadder.tsx
@@ -1,32 +1,47 @@
 import React from "react"
-import { Table } from "antd"
-import { GamerDungeon } from "./types"
-import { ColumnsType } from "antd/es/table"
+import {Table} from "antd"
+import {GamerDungeon} from "./types"
+import {ColumnsType} from "antd/es/table"
 
 interface DungeonLadderProps {
-  gamersDungeon: GamerDungeon[]
-  loading: boolean
-  caption: string
+    gamersDungeon: GamerDungeon[]
+    loading: boolean
+    caption: string
 }
-export const DungeonLadder = (props: DungeonLadderProps) => {
-  const columns: ColumnsType<GamerDungeon> = [
-    { title: "Name", dataIndex: "name" },
-    { title: "Tyrannical Level", dataIndex: "tyrannicalLevel", sorter:(a,b) => a.tyrannicalLevel.localeCompare(b.tyrannicalLevel) },
-    { title: "Fortified Level", dataIndex: "fortifiedLevel", sorter:(a,b) => a.fortifiedLevel.localeCompare(b.fortifiedLevel) },
-    { title: "Score", dataIndex: "score", sorter: (a,b) => a.score -b.score}
-  ]
 
-  return (
-    <div>
-      <Table
-          columns={columns}
-          dataSource={props.gamersDungeon}
-          pagination={false}
-          showHeader={true}
-          bordered={true}
-          caption={<div className={"font-face-lc"}><h1>{props.caption}</h1></div>}
-          loading={props.loading}
-      />
-    </div>
-  )
+export const DungeonLadder = (props: DungeonLadderProps) => {
+    const columns: ColumnsType<GamerDungeon> = [
+        {title: "Name", dataIndex: "name"},
+        {
+            title: "Tyrannical Level",
+            dataIndex: "tyrannicalLevel",
+            sorter: (a, b) => a.tyrannicalLevel.localeCompare(b.tyrannicalLevel),
+            render: (text, record) => (
+                <a href={record.tyrannicalUrl}>{text}</a>
+            ),
+        },
+        {
+            title: "Fortified Level",
+            dataIndex: "fortifiedLevel",
+            sorter: (a, b) => a.fortifiedLevel.localeCompare(b.fortifiedLevel),
+            render: (text, record) => (
+                <a href={record.fortifiedUrl}>{text}</a>
+            ),
+        },
+        {title: "Score", dataIndex: "score", sorter: (a, b) => a.score - b.score}
+    ]
+
+    return (
+        <div>
+            <Table
+                columns={columns}
+                dataSource={props.gamersDungeon}
+                pagination={false}
+                showHeader={true}
+                bordered={true}
+                caption={<div className={"font-face-lc"}><h1>{props.caption}</h1></div>}
+                loading={props.loading}
+            />
+        </div>
+    )
 }
--- src/features/dungeon/types.ts
+++ src/features/dungeon/types.ts
@@ -3,7 +3,9 @@ import {MythicPlusRun, RaiderioProfile} from "../../utils/raiderio";
 export interface GamerDungeon {
   name: string
   tyrannicalLevel: string
+  tyrannicalUrl: string
   fortifiedLevel: string
+  fortifiedUrl: string
   score: number
 }
 
@@ -22,8 +24,10 @@ export function gamerDungeonFromRaiderioProfile(raiderioProfile: RaiderioProfile
   return {
     name: raiderioProfile.name,
     tyrannicalLevel: (bestTyrannicalDungeon?.mythic_level.toString() ?? "") + numberToPlusString(bestTyrannicalDungeon?.num_keystone_upgrades ?? 0),
+    tyrannicalUrl: bestTyrannicalDungeon?.url ?? "",
     fortifiedLevel: (bestFortifiedDungeon?.mythic_level.toString() ?? "") +  numberToPlusString(bestFortifiedDungeon?.num_keystone_upgrades ?? 0),
+    fortifiedUrl: bestFortifiedDungeon?.url ?? "",
     score: Number(((bestDungeon?.score ?? 0) * 1.5
-        +  (bestAlternateDungeon?.score ?? 0) * 0.5).toFixed(1))
+        +  (bestAlternateDungeon?.score ?? 0) * 0.5).toFixed(1)),
   }
 }
