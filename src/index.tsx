4cd58047055160c89f494faeb453f1ec203921a2
Sancho, Xavier <xavier.sanchoruiz@sky.uk>
2026-04-20 09:49:14 +0100

    sanxei95 - * case where deleting all character from a view, it showed sync characters views modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { store } from "@/app/store";
+import { queryClient } from "@/app/queryClient";
 import App from "@/app/App";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -9,8 +11,10 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </Provider>,
 );
